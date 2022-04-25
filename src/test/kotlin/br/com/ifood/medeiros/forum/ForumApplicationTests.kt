2e6505fca856b7da58972af67b40562abd7c9133
lucas.medeiros <lucas.medeiros@ifood.com.br>
2022-04-25 14:04:08 -0300

    chore: initial commit

---
+++ src/test/kotlin/br/com/ifood/medeiros/forum/ForumApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.ifood.medeiros.forum
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
