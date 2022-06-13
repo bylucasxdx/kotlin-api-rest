a1ee6fb61d62e435231ee7c9eaaf3973b11c9c0c
lucas.medeiros <lucas.medeiros@ifood.com.br>
2022-06-13 12:01:46 -0300

    test: add mockmvc to execute api tests

---
+++ src/test/kotlin/br/com/ifood/medeiros/forum/controller/TopicControllerTest.kt
@@ -0,0 +1,34 @@
+package br.com.ifood.medeiros.forum.controller
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TopicControllerTest {
+
+    @Autowired
+    private lateinit var webApplicationContext: WebApplicationContext
+
+    private lateinit var mockMvc: MockMvc
+
+    @BeforeEach
+    fun setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .apply<DefaultMockMvcBuilder?>(
+                SecurityMockMvcConfigurers.springSecurity()
+            ).build()
+    }
+
+    @Test
+    fun `Should return a 200 status code when call get topics`() {
+        mockMvc.get("/topics").andExpect { status { is2xxSuccessful() } }
+    }
+}
